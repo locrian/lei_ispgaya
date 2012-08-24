b8b4dc650958a72afbeb52f7d3408ab85b99364a
ricardo.taboada@hotmail.com <rft@mint>
2012-08-24 20:14:02 +0100

    Starting the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeiIspgaya::Application.config.secret_token = '414c16a967748e6cf244fe90797692e30c5c3e9ebd3fd827dde4ba1343598b09feb3a36dba713a2e5a4e694ef24de9021678cc41b97b33873b9aaa1fdbec0b2f'
